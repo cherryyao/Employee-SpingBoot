2997fa1d3a950abb72e71e6530564727979336d5
YAOCH2 <christine.s.yao@oocl.com>
2018-07-24 13:11:08 +0800

    add EmployeeService

---
+++ src/main/java/com/example/demo/Dao/EmployeeDao.java
@@ -0,0 +1,9 @@
+package com.example.demo.Dao;
+
+import com.example.demo.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao {
+    List<Employee> getAllEmployees();
+}
--- src/main/java/com/example/demo/Service/EmployeeService.java
+++ src/main/java/com/example/demo/Service/EmployeeService.java
@@ -1,11 +1,18 @@
 package com.example.demo.Service;
 
+import com.example.demo.Dao.EmployeeDao;
 import com.example.demo.model.Employee;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class EmployeeService {
+@Component
+public class EmployeeService implements EmployeeDao {
+    @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        List<Employee> list  = new ArrayList<>();
+        list.add(new Employee(1,"Tom",21,"男"));
+        return list;
     }
 }
