648bd40a34ad2fb759b64169997ee184df65c26b
YAOCH2 <christine.s.yao@oocl.com>
2018-07-24 12:54:43 +0800

    create controller model service

---
+++ src/main/java/com/example/demo/Service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.example.demo.Service;
+
+import com.example.demo.model.Employee;
+
+import java.util.List;
+
+public class EmployeeService {
+    public List<Employee> getAllEmployees() {
+        return null;
+    }
+}
